078d1d3e6fcf11599d7b55c8080020e0dd7e45d1
Kiriill <op.96@mail.ru>
2016-05-15 00:56:09 +0700

    Heroku v4

--- AlibabaStudio/settings.py
+++ AlibabaStudio/settings.py
@@ -124,7 +124,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
