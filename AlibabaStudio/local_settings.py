d75089158e6a487f62efdbec041651575d52ca60
Kiriill <op.96@mail.ru>
2016-05-15 15:54:02 +0700

    day 2, v4

--- AlibabaStudio/local_settings.py
+++ AlibabaStudio/local_settings.py
@@ -24,8 +24,8 @@ DATABASES = {
         'NAME': 'vipmaker_test3',
         'USER': 'root',
         'PASSWORD': 'qazzaq123',
-        # 'HOST': '127.0.0.1',
-        # 'PORT': '3306',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
