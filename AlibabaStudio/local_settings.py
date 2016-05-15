03631911e80502da80612201fd6d6dd4309285ba
Kiriill <op.96@mail.ru>
2016-05-15 16:20:53 +0700

    day 2, v7

--- AlibabaStudio/local_settings.py
+++ AlibabaStudio/local_settings.py
@@ -18,16 +18,14 @@ TEMPLATE_DEBUG = True
 # }
 
 
-# mysql://bf329ce360970a:ebb86e20@eu-cdbr-west-01.cleardb.com/heroku_bfee0306a62d481?reconnect=true
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'heroku_bfee0306a62d481',
-        'USER': 'bf329ce360970a',
-        'PASSWORD': 'ebb86e20',
-        # 'HOST': 'localhost',
-        # 'PORT': '3306',
+        'NAME': 'vipmaker_db',
+        'USER': 'dbuser',
+        'PASSWORD': 'qwerty123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
