0a1134755e905d24be511baa1e2c68ebfe647458
Kiriill <op.96@mail.ru>
2016-05-18 02:21:15 +0700

    day 5 v3

--- AlibabaStudio/settings.py
+++ AlibabaStudio/settings.py
@@ -124,7 +124,7 @@ EMAIL_FILE_PATH = 'tmp/messages/'
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 # STATIC_URL = '/static/'
 # MEDIA_URL = '/uploads/'
 
